035a589930702969b041b0e59701d54830f18a59
ogwata-test <71211063+ogwata-test@users.noreply.github.com>
2022-08-14 13:39:38 +0900

    Update vivliostyle.config.js

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'tbf13-draft', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
-  author: 'spring-raining <harusamex.com@gmail.com>', // default to `author` in `package.json` or undefined.
+  author: '小形克宏 <ogwata@vivliostyle.org>', // default to `author` in `package.json` or undefined.
   language: 'ja', // default to undefined.
-  // size: 'A4', // paper size.
+  //size: '', // paper size.
   theme: 'theme', // .css or local dir or npm package. default to undefined.
-  entry: ['content/index.md'],
+  entry: ['content/ogwata/draft-ogwata.md'],
   // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
   output: [
-    './tbf13-draft.pdf', // the output format will be inferred from the name.
+    './draft-ogwata.md.pdf', // the output format will be inferred from the name.
   ],
   // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
